8ddbbb55f6b9f3052d4fad111dc594178dab6a93
deogo <diogogpontes@gmail.com>
2024-11-22 09:13:24 -0300

    Atualização no db.js inserindo funções e realizando inserção no banco de dados

--- db.js
+++ db.js
@@ -1,28 +1,52 @@
-require('dotenv').config();
-const {MongoClient} = require('mongodb');
-
-let singleton;
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'AgendaTaxi';
+const connectionString = url + '/' + dbName;
+const client = new MongoClient(connectionString);
 
 async function connect() {
-
-    if (singleton) {
-        return singleton;
-        console.log('Servidor conectado');
-        
-    }
-    const client = new MongoClient(process.env.MONGODB_HOST);
-    await client.connect();
-
-    client.db(process.env.MONGODB_DATABASE);
-    singleton = client.db(process.env.MONGODB_DATABASE);
-    return singleton;
+  await client.connect();
+  console.log('Conectado ao banco de dados!');
 }
 
-async function insert(customer){
-    const db = await connect();
-    return db.collection('customers').insertOne(customer);
+async function close() {
+  await client.close();
+  console.log('Conexão com o banco de dados fechada!');
+}
+
+async function insert(customer) {
+  const db = client.db(dbName);
+  const collection = db.collection('customers');
+  const result = await collection.insertOne(customer);
+  return result;
+}
+
+async function read() {
+  const db = client.db(dbName);
+  const collection = db.collection('customers');
+  const customers = await collection.find().toArray();
+  return customers;
+}
+
+async function update(id, customer) {
+  const db = client.db(dbName);
+  const collection = db.collection('customers');
+  const result = await collection.updateOne({_id: id}, {$set: customer});
+  return result;
+}
+
+async function remove(id) {
+  const db = client.db(dbName);
+  const collection = db.collection('customers');
+  const result = await collection.deleteOne({_id: id});
+  return result;
 }
 
 module.exports = {
-    insert
-}
+  connect,
+  close,
+  insert,
+  read,
+  update,
+  remove,
+};
