f96d152f3495c5ccff1e983b9fc1dba5b0833f19
deogo <diogogpontes@gmail.com>
2024-11-22 08:27:56 -0300

    Atualizando arquivo de conexão

--- db.js
+++ db.js
@@ -1,11 +1,28 @@
+require('dotenv').config();
 const {MongoClient} = require('mongodb');
 
+let singleton;
+
 async function connect() {
+
+    if (singleton) {
+        return singleton;
+        console.log('Servidor conectado');
+        
+    }
     const client = new MongoClient(process.env.MONGODB_HOST);
     await client.connect();
 
     client.db(process.env.MONGODB_DATABASE);
+    singleton = client.db(process.env.MONGODB_DATABASE);
+    return singleton;
+}
 
-    const db = client.db('test');
-    return db;
+async function insert(customer){
+    const db = await connect();
+    return db.collection('customers').insertOne(customer);
+}
+
+module.exports = {
+    insert
 }
